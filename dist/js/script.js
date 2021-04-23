2da73ca9cf9515ad8e5e55dbd588ceef4d341d7e
Takhir <taxup91@gmail.com>
2021-04-23 19:53:33 +0600

    three commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,13 +3,31 @@ window.addEventListener('DOMContentLoaded', function() {
     new Swiper('.slider', {
         //Arrow
         navigation: {
-            nextEl: '.slider__next', //Кнопка next
-            prevEl: '.slider__prev' //Кнопка prev
+            nextEl: '.slider__arrows-next', //Кнопка next
+            prevEl: '.slider__arrows-prev' //Кнопка prev
         },
         
         slidesPerView: 4, //Количество выводимых слайдов
         spaceBetween: 20, //Отступы между слайдами
         loop: true, //Бесконечная прокрутка
+
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+
+            993: {
+                slidesPerView: 2,
+            },
+
+            1200: {
+                slidesPerView: 3,
+            },
+
+            1360: {
+                slidesPerView: 4
+            }
+        },
     });
 
 });
